77266bf594ec0bcf917c16b2a2680e71747b435e
Jonathan Pipping <jdpipping@gmail.com>
2023-07-17 15:29:41 -0400

    more data cooking
    standardization of speed, acceleration

--- data-cooking.R
+++ data-cooking.R
@@ -5,7 +5,10 @@ partial_horse_data <- read_csv('processed-horse-data/partially_cleaned_data.csv'
 
 # standardizing velocity, acceleration within races ####
 std_horse_data <- partial_horse_data %>%
-  group_by(race_id) %>%
+  group_by(race_id, frame_id) %>%
   mutate(std_speed <- as.vector(scale(speed)),
-         std_acceleration <- as.vector(scale(acceleration)))
+         std_acceleration <- as.vector(scale(acceleration))) %>%
+  ungroup()
 write_csv(std_horse_data, 'processed-horse-data/standardized_partial_data.csv')
+
+# 
